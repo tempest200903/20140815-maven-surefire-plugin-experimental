d01412e14212e90bf6d10eab92515f004dc7fded
tempest200903 <tempest200903@gmail.com>
2014-08-15 02:01:28 +0900

    JUnit 4.8 @Category アノテーション を試す

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,38 +1,47 @@
 package com.example;
 
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest extends TestCase {
+	/**
+	 * Create the test case
+	 * 
+	 * @param testName
+	 *            name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+	@Test
+	public void testApp() {
+		System.out.println("testApp");
+		assertTrue(true);
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	@Test
+	@Category(com.example.SlowTests.class)
+	public void testSlow() {
+		System.out.println("testSlow");
+		assertTrue(true);
+	}
+
+	@Test
+	@Category(com.example.SlowerTests.class)
+	public void testSlower() {
+		System.out.println("testSlower");
+		assertTrue(true);
+	}
+
+	// @Test
+	// @Category(com.example.FastTests.class)
+	// public void testSlow() {
+	// System.out.println("fast");
+	// }
+
 }
---
+++ src/test/java/com/example/SlowerTests.java
@@ -0,0 +1,5 @@
+package com.example;
+
+public interface SlowerTests extends SlowTests {
+
+}
